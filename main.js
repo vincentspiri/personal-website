5f73ef423dd956d5194faba567effb6d7c7e86df
Vincent Spiri <vincentspiri@yahoo.com>
2023-01-13 13:51:44 -0600

    Clean up

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('asdasd')
+const squareList = arr => {
+    const newArr = arr.filter(num => {
+      return num * 10 % 10 == 0 && num > 0;
+    }).map(num => num*num)
+    return newArr;
+  };
+  
+  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+  console.log(squaredIntegers);
