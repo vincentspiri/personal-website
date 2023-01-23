add29fd3d5e7282987030c033f88ab580734dc41
Vincent Spiri <vincentspiri@yahoo.com>
2023-01-23 03:36:05 -0600

    Project section hover effect

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const squareList = arr => {
-    const newArr = arr.filter(num => {
-      return num * 10 % 10 == 0 && num > 0;
-    }).map(num => num*num)
-    return newArr;
-  };
-  
-  const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
-  console.log(squaredIntegers);
+const ppbutton = document.querySelector("#ppbutton");
+const h1 = document.querySelector("#hello");
+const p = document.querySelectorAll(".pp");
+const buttons = document.querySelectorAll("button");
+const test = document.querySelector(".test");
+test.src = "https://media.istockphoto.com/id/1368264124/photo/extreme-close-up-of-thrashing-emerald-ocean-waves.jpg?b=1&s=170667a&w=0&k=20&c=qha_PaU54cu9QCu1UTlORP4-sW0MqLGERkdFKmC06lI=";
+console.log(test.attribute);
+console.log(buttons);
+
+console.log(p[1]);
+
+ppbutton.onclick = () => {
+    h1.innerText = "penis";
+};
